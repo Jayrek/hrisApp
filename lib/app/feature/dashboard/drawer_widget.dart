bed1ebac5fdb83481b150f901e106047d49321f3
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-13 22:35:53 +0800

    add: apply the user name in drawer_widget.dart

--- lib/app/feature/dashboard/drawer_widget.dart
+++ lib/app/feature/dashboard/drawer_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rgs_hris/app/bloc/auth/auth_bloc.dart';
 
 import '../../../router/app_route.dart';
 
-class DrawerWidet extends StatelessWidget {
-  const DrawerWidet({super.key});
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,82 +15,99 @@ class DrawerWidet extends StatelessWidget {
         child: ListView(
           physics: const BouncingScrollPhysics(),
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.grey.shade300,
-                    radius: 40,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'DELA CRUZ, SANTOS, JUANA MARIA',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            _buildDrawerHeader(),
             Divider(
               height: 1,
               color: Colors.grey.withOpacity(0.5),
             ),
-            drawerList(context)
+            _buildDrawerList(context)
           ],
         ));
   }
 
-  Widget drawerList(BuildContext context) {
+  Widget _buildDrawerHeader() {
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is AuthSignInSuccess) {
+          final employeeData = state.loginWrapperResponse.loginResponse
+              ?.loginDataResponse?.employeeDetails;
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.blue.shade200,
+                  radius: 40,
+                  child: CircleAvatar(
+                    radius: 38,
+                    backgroundColor: Colors.grey.shade300,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    '${employeeData?.name.toString()}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w800),
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+        return const SizedBox();
+      },
+    );
+  }
+
+  Widget _buildDrawerList(BuildContext context) {
     return Column(
       children: [
-        drawerItem(Icons.person, 'PERSONAL', onTap: () {
+        _buildDrawerItem(Icons.person, 'PERSONAL', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.personal.name);
         }),
-        drawerItem(Icons.person_add_alt_sharp, 'EMPLOYMENT', onTap: () {
+        _buildDrawerItem(Icons.person_add_alt_sharp, 'EMPLOYMENT', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.employment.name);
         }),
-        drawerItem(Icons.work, 'WORK', onTap: () {
+        _buildDrawerItem(Icons.work, 'WORK', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.work.name);
         }),
-        drawerItem(Icons.note, 'LEAVE', onTap: () {
+        _buildDrawerItem(Icons.note, 'LEAVE', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.leaves.name);
         }),
-        drawerItem(Icons.access_time, 'ATTENDANCE', onTap: () {
+        _buildDrawerItem(Icons.access_time, 'ATTENDANCE', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.attendance.name);
         }),
-        drawerItem(Icons.timeline_outlined, 'PERFORMANCE', onTap: () {
+        _buildDrawerItem(Icons.timeline_outlined, 'PERFORMANCE', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.performance.name);
         }),
-        drawerItem(Icons.note_outlined, 'DOCUMENTS', onTap: () {
+        _buildDrawerItem(Icons.note_outlined, 'DOCUMENTS', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.documents.name);
         }),
-        drawerItem(Icons.notes_rounded, 'RGS HANDBOOK', onTap: () {
+        _buildDrawerItem(Icons.notes_rounded, 'RGS HANDBOOK', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.handbook.name);
         }),
-        drawerItem(Icons.note_alt_sharp, 'REQUEST UPDATE', onTap: () {
+        _buildDrawerItem(Icons.note_alt_sharp, 'REQUEST UPDATE', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.requestUpdate.name);
         }),
-        drawerItem(Icons.account_circle, 'MY ACCESS', onTap: () {
+        _buildDrawerItem(Icons.account_circle, 'MY ACCESS', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.myAccess.name);
         }),
-        drawerItem(Icons.key, 'CHANGE PASSWORD', onTap: () {
+        _buildDrawerItem(Icons.key, 'CHANGE PASSWORD', onTap: () {
           Navigator.of(context).pop();
           context.pushNamed(AppRoute.changePassword.name);
         }),
@@ -96,7 +115,7 @@ class DrawerWidet extends StatelessWidget {
     );
   }
 
-  Widget drawerItem(IconData icon, String title, {Function()? onTap}) {
+  Widget _buildDrawerItem(IconData icon, String title, {Function()? onTap}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 5),
       child: ListTile(
