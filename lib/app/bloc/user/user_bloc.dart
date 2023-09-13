16cbed994fde81d515373300b18e17a4869ba4fc
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-13 22:32:24 +0800

    add: bloc classes related to personal

--- lib/app/bloc/user/user_bloc.dart
+++ lib/app/bloc/user/user_bloc.dart
@@ -1,15 +1,32 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rgs_hris/core/data/model/response/personal_wrapper_response.dart';
+
+import '../../../core/domain/manager/token_manager.dart';
+import '../../../core/domain/repository/user/user_repository.dart';
 
 part 'user_event.dart';
+
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
-    on<UserEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserRepository userRepository;
+
+  UserBloc({required this.userRepository}) : super(UserInitial()) {
+    on<UserFetched>(_onPersonalFetched);
+  }
+
+  FutureOr<void> _onPersonalFetched(
+    UserFetched event,
+    Emitter<UserState> emit,
+  ) async {
+    final tokenValue = await TokenManager.getToken();
+    final response =
+        await userRepository.getPersonalInformation(token: tokenValue);
+
+    print('personalResponse $response');
+    emit(UserLoaded(personalWrapperResponse: response));
   }
 }
--- lib/app/bloc/user/user_event.dart
+++ lib/app/bloc/user/user_event.dart
@@ -2,4 +2,9 @@ part of 'user_bloc.dart';
 
 abstract class UserEvent extends Equatable {
   const UserEvent();
+  @override
+  List<Object?> get props => [];
 }
+
+
+class UserFetched extends UserEvent{}
