9efbb4a4bdf1db3bb6350245dce9345337cac5a3
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-14 11:47:06 +0800

    chore: fix git guardian secret failed

--- lib/core/data/dio/dio_interceptor.dart
+++ lib/core/data/dio/dio_interceptor.dart
@@ -10,7 +10,7 @@ class DioInterceptor extends Interceptor {
     options.headers.addAll({
       HttpHeaders.acceptHeader: 'application/json',
       HttpHeaders.contentTypeHeader: 'application/json',
-      HttpHeaders.authorizationHeader: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOjIsImlzcyI6Im9ubGluZWhyaXMiLCJpYXQiOjE2OTQyNTA5MjcsImV4cCI6MTY5NDMxMDkyN30.lww7lUK4lUP4O3ZvLM4CHKi8AQkx2_uc14zwBZSKrNoUVqm2bc-N3DjhdIaO5s-CJW4CIwkWc9xhCjmqFXEqJWCO_DxscxAMs7FS2bB6IDeRWMdKHd55zIDfnT5FdbhMYp9mZyAd0PZ8WT66o_MxWVJQ4znwNVnhqW5YuNgroPM'
+      // HttpHeaders.authorizationHeader: 'Bearer $token'
     });
     debugPrint('REQUEST [${options.method}] => ${options.uri}');
     debugPrint('REQUEST DATA => ${jsonEncode(options.data)}');
