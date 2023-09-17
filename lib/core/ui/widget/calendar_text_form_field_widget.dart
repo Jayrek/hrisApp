4c4d1a98ea4180db1688aa69b48668784ca1120f
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-17 15:58:40 +0800

    add: custom widget for calendar field and drop down field

---
+++ lib/core/ui/widget/calendar_text_form_field_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class CalendarTextFormFieldWidget extends StatelessWidget {
+  const CalendarTextFormFieldWidget({
+    required this.name,
+    required this.hint,
+    this.onTap,
+    super.key,
+  });
+
+  final String name;
+  final String hint;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+      name: name,
+      readOnly: true,
+      decoration: InputDecoration(
+        suffixIcon: const Icon(
+          Icons.calendar_month,
+          size: 20,
+        ),
+        hintText: hint,
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20,
+        ),
+        isDense: true,
+        hintStyle: TextStyle(
+            fontSize: 12,
+            color: Colors.grey.shade400,
+            fontWeight: FontWeight.bold),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade100,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade300,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(50),
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade100,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(50),
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade100,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(50),
+          ),
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+}
