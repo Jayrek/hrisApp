4236900ca0a3db2802ae02663bf12d4e144e02d3
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-17 12:33:58 +0800

    add: custom widget_util.dart

---
+++ lib/core/ui/widget/widget_util.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WidgetUtil {
+  static Widget customDivider() => Divider(
+        height: 1,
+        color: Colors.grey.withOpacity(0.5),
+      );
+}
