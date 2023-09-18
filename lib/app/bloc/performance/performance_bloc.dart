c1516257b5f9b7a9e652095fc3e4fff0db240b9d
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-09-18 18:00:36 +0800

    add: performance bloc files

---
+++ lib/app/bloc/performance/performance_bloc.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rgs_hris/core/data/model/response/goals_wrapper_response.dart';
+import 'package:rgs_hris/core/data/model/response/performance_wrapper_response.dart';
+import 'package:rgs_hris/core/domain/manager/shared_prefs_manager.dart';
+
+import '../../../core/data/repository/performance/performance_repository_impl.dart';
+import '../../common/util/key_strings.dart';
+
+part 'performance_event.dart';
+
+part 'performance_state.dart';
+
+class PerformanceBloc extends Bloc<PerformanceEvent, PerformanceState> {
+  final PerformanceRepositoryImpl performanceRepository;
+
+  PerformanceBloc({required this.performanceRepository})
+      : super(PerformanceInitial()) {
+    on<PerformanceProfileFetched>(_onPerformanceProfileFetched);
+    on<PerformanceGoalsFetched>(_onPerformanceGoalsFetched);
+  }
+
+  FutureOr<void> _onPerformanceProfileFetched(
+    PerformanceProfileFetched event,
+    Emitter<PerformanceState> emit,
+  ) async {
+    emit(PerformanceLoading());
+    final tokenValue =
+        await SharedPrefsManager().getStringPref(KeyStrings.spTokenKey);
+    final response = await performanceRepository.getPerformanceInformation(
+        token: tokenValue);
+
+    emit(PerformanceProfileLoaded(performanceWrapperResponse: response));
+  }
+
+  FutureOr<void> _onPerformanceGoalsFetched(
+    PerformanceGoalsFetched event,
+    Emitter<PerformanceState> emit,
+  ) async {
+    emit(PerformanceLoading());
+    final tokenValue =
+        await SharedPrefsManager().getStringPref(KeyStrings.spTokenKey);
+    final response = await performanceRepository.getPerformanceGoalsInformation(
+        token: tokenValue);
+
+    emit(PerformanceGoalsLoaded(goalsWrapperResponse: response));
+  }
+}
